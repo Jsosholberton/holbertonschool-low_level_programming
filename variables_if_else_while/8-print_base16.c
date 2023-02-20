f5f6e716f2bddfb4ba02d355f0d6164157f262f1
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-20 13:13:31 -0800

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+char n;
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+for (l = 'a'; l <= 'f'; l++)
+putchar(l);
+putchar('\n');
+return (0);
+}
