956a60e3c7cbf5c95880db94a9ec68d5f60053c4
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-21 05:34:22 -0800

    abc x 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - abc x 10
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+int abc;
+int n = 1;
+while (n++ <= 10)
+{
+for(abc = 'a'; abc <= 'z'; abc++)
+_putchar(abc);
+_putchar('\n');
+}
+}
