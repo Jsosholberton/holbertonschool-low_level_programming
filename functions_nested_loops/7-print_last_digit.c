11c2020d9771f92a25f33c0224a4dae15709e660
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-22 08:59:35 -0800

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_print_last_digit: print the last digit
+ *@r: variable initial
+ *@t: variable positive
+ *_putchar: print the last digit
+ *return: ever 0
+ */
+int print_last_digit(int r)
+{
+int t = r % 10;
+if (t <= 0)
+t *= -1;
+_putchar (t + '0');
+return (0);
+}
