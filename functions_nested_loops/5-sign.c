5c3a2f7b47fe7917bc4fcf3bd73ff1beaf526512
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-22 08:13:07 -0800

    sing

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sing - print the sing of the digit
+ * @n: digit variable
+ *
+ *
+ */
+int print_sign(int n)
+{
+  if (n > 0)
+    {
+      _putchar ('+');
+      return (1);
+    }
+  else if  (n == 0)
+    {
+      _putchar ('0');
+      return (0);
+    }
+  else
+    {
+    _putchar ('-');
+    return (-1);
+    }
+}
