f98696fbe0ef7aa404dce80e5153352523fbdf70
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-22 08:45:12 -0800

    absolute

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * _abs: print the valor absolute
- * @r: is the variable usade
- * return: -r if is negative, return r if is positive, _abs return 
- *
- *
+ * _abs: print the valor absolute.
+ * @r: is the variable usade.
+ * return: -r if is negative, return r if is positive, _abs return.
  */
 int _abs(int r)
 {
