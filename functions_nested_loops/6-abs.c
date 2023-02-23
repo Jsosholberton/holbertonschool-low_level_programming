5b582c2ac1c40ee0e98f1d1568e87c711cdcfb05
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 12:02:38 -0800

    betty style

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,7 +2,10 @@
 /**
  * _abs: print the valor absolute.
  * @r: is the variable usade.
- * return: -r if is negative, return r if is positive, _abs return.
+ *
+ * Description: return the valor absolute
+ * header: print the respose
+ * Return: -r if is negative, return r if is positive, _abs return.
  */
 int _abs(int r)
 {
