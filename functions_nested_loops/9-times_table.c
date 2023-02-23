47afba1380a9b94d99b298285040471cde729d62
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 12:19:40 -0800

    betty style

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * main - check the code
+ * times_table - check the code
+ * @n, @z, @y, variables to work
  *
+ * Description: times table
+ * header: print the response
  * Return: always 0.
- * times_table - Prins the table
  *
  */
 void times_table(void)
 {
 int n, z, y;
 
-for (n = 0;n <= 9; n++)
+for (n = 0; n <= 9; n++)
 {
-_putchar('0');
- 
+_putchar('0'); 
 for (z = 1; z <= 9; z++)
 {
 _putchar(',');
@@ -25,7 +26,6 @@ if (y <= 9)
 _putchar(' ');
 else
 _putchar((y / 10) + '0');
- 
 _putchar((y % 10) + '0');
 }
 _putchar('\n');
