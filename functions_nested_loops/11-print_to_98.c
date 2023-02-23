f6374e59cf83bf31636a725975845164c62a2e8b
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 12:34:47 -0800

    betty style

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * print_to_98 - print the number ultil 98
+ * @n: variable
  *
  */
 void print_to_98(int n)
 {
-  if (n < 98)
-    {
-      while (n < 99)
-	printf("%d, ", n++);
-	}
-  else
-    {
-      while (n > 99)
-	printf("%d, ", n--);
-      printf("%d\n", n);
-    }
+if (n < 98)
+{
+while (n < 98)
+printf("%d, ", n++);
+}
+else
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
 }
--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * jack_bauer - time of ocloc 00:00 -> 23:59
- * @h: hours, @m: minuts
+ * jack_bauer - time of oclock 00:00 -> 23:59
+ * @h: hours
+ * @m: minuts
  *
  * Description: print the time minute to minute until 23:59
  * header: print the response
@@ -10,12 +11,9 @@
 void jack_bauer(void)
 {
 int h, m;
-
-for (h = 0;h <= 23;h++)
-  
+for( h = 0; h <= 23; h++)
 {
-for (m = 0;m <= 59;m++)
-  
+for ( m = 0; m <= 59; m++)
 {
 _putchar ((h / 10) + '0');
 _putchar ((h % 10) + '0');
