3ef7de22a4cf741c10c6e573269eedda05ac80d1
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 12:26:25 -0800

    betty style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * add - addition
- * @a, @b variables to addition
+ * @a: variable 1, @b: variable 2.
  *
  * Return: return the result
  */
 int add(int a, int b)
 {
-return(a + b);
+return (a + b);
 }
