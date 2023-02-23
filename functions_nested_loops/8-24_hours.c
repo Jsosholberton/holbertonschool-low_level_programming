60fb7be5098afa28ed55a5cee35d2ac5e8bafe97
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 12:12:13 -0800

    betty style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - time of ocloc 00:00 -> 23:59
- *@h: hours, @m: minuts
+ * @h: hours, @m: minuts
  *
  * Description: print the time minute to minute until 23:59
  * header: print the response
@@ -12,8 +12,10 @@ void jack_bauer(void)
 int h, m;
 
 for (h = 0;h <= 23;h++)
+  
 {
 for (m = 0;m <= 59;m++)
+  
 {
 _putchar ((h / 10) + '0');
 _putchar ((h % 10) + '0');
