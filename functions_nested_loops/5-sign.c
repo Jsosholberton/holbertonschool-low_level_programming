76ff137df4f2f3aca7734bd11d057e85bb5dc9a9
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 11:58:02 -0800

    betty style

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * print_sing - print the sing of the digit
+ * print_sign - print the sing of the digit
  * @n: digit variable
  *
- * return -  1 if is positive, 0 if is zero, and -1 if is negative
- *
+ * Description: print if is positive + and return 1, if is negative print - and return -1, if is 0 print 0 and return 0
+ * header:  1 if is positive, 0 if is zero, and -1 if is negative
+ * Return: return 1 if is positive, -1 if is negative, 0 if is 0
  */
 int print_sign(int n)
 {
