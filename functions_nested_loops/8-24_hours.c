407fc83c45d4458f4afac32d92293d1564abcd1b
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-23 12:37:49 -0800

    betty style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@
  * Return: no value
  */
 void jack_bauer(void)
-{
 int h, m;
-for( h = 0; h <= 23; h++)
 {
-for ( m = 0; m <= 59; m++)
+for (h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
 {
 _putchar ((h / 10) + '0');
 _putchar ((h % 10) + '0');
