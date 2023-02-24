028576bb4363fa3ff85a55a9a989a9ff4d0a2908
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-24 12:54:59 -0800

    issuper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - 1 and 0
+ * @c: is a variable
+ *
+ * Description: if the character is a lleter return 1, else 0
+ * header: _isalpha return 1 if is true, 0 if is false
+ * Return: return 1 if is true 0 if is false
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return(0);
+}
