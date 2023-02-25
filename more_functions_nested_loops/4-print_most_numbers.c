e23dc9520c692238331c818d7de43bc9ba2f7f33
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-24 18:36:19 -0800

    print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+char n;
+for (n = 0; n <=9; n++)
+{
+if (n != 2 && n != 4)
+  _putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
