3d5e4de05f90e01817b3f6ce06fab2bdb83f35d2
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-24 18:27:52 -0800

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print the number from 0 to 9
+ * @n: varibale
+ *
+ * Return: always 0
+ *
+ */
+void print_numbers(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+_putchar (n);
+_putchar ('\n');
+}
