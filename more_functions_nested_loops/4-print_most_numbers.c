813a28a27c6f3d7221eb1bf1e239b7b09ab36ea9
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-24 18:38:20 -0800

    print most numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
+ * print_most_numbers - print the numbers from 0 to 9 excep 2 and 4
  *
- *
- *
- *
- *
+ * Return: no value
  */
 void print_most_numbers(void)
 {
@@ -12,7 +10,7 @@ char n;
 for (n = 0; n <=9; n++)
 {
 if (n != 2 && n != 4)
-  _putchar((n % 10) + '0');
+_putchar((n % 10) + '0');
 }
 _putchar('\n');
 }
