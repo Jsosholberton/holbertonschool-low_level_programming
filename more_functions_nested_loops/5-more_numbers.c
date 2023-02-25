e158870db782f7c781dde55dd301260ced60e83c
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-24 19:02:28 -0800

    more numbers 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times the number from 0 to 14
+ *
+ * Return: no data
+ */
+void more_numbers(void)
+{
+int c=1, n;
+while (c++ <= 10)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+}
