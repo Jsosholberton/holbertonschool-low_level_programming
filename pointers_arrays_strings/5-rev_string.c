10e92e3fb74d238fefc9fde9299a69f7c28b6d39
Johnatan Ortiz <6403@holbertonstudents.com>
2023-02-28 17:20:55 -0800

    rev string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - prints a string in reverse
+ * @s: character to reverse
+ *
+ * Return: always 0
+ */
+void rev_string(char *s)
+{
+int a, b = 0, c;
+char t;
+ for (a = 0; s[a] != '\0'; a++);
+c = a / 2;
+while (c--)
+{
+t = s[a - b - 1];
+s[a - b - 1] = s[b];
+s[b] = t;
+b++;
+}
+}
