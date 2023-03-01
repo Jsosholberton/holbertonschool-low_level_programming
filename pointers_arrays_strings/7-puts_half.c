c6c75830e8f93482523572fdba20dd0df008e245
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-01 06:58:48 -0800

    puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - print half of a string
+ * @str: is the string
+ *
+ */
+void puts_half(char *str)
+{
+  int c = 0, n, t;
+while (str[c])
+{
+c++;
+}
+t = c % 2;
+if (t == 0)
+{
+n = c / 2;
+while (n <= c)
+{
+_putchar(str[n]);
+n++;
+}
+}
+else
+{
+n = (c - 1) / 2;
+while (n <= c)
+{
+_putchar(str[n]);
+n++;
+}
+} 
+_putchar('\n');
+}
