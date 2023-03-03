4e0c83ca365af6f84a6a9c3a030559b15b5b79a1
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-03 12:24:31 -0800

    cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string 
+ *
+ * Return: the result
+ */
+char *cap_string(char *str)
+{
+int c = 0;
+while (str[c])
+{
+  while (!(str[c] >= 97 && str[c] <= 122))
+    c++;
+  if (str[c - 1] == ' ' || str[c - 1] == '\t' || str[c - 1] == '\n' || str[c - 1] == ',' || str[c - 1] == ';' || str[c - 1] == '.' || str[c - 1] == '!' || str[c - 1] == '?' || str[c - 1] == '"' || str[c - 1] == '(' || str[c - 1] == ')' || str[c - 1] == '{' || str[c - 1] == '}' || c == 0)
+    str[c] -= 32;
+  c++;
+ }
+ return(str);
+}
