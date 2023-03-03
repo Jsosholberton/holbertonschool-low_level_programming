c7e2bc6c6098d5775a8c741c52fc0bfb4dab5fd3
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-03 10:42:28 -0800

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,23 @@
  * Return: the result of s1 - s2 in ASCII
  */
 int _strcmp(char *s1, char *s2)
-{
+/* {
 int c = 0, result = 0;
-while (s1[c] && s2[c] != '\0')
+while (s1[c] != '\0' && s2[c] != '\0')
 {
 result += s1[c] - s2[c];
 c++;
 }
 return (result);
+}
+*/
+{
+while (*s1 != '\0' || *s2 != '\0')
+{
+if (*s1 != *s2)
+return (*s1 - *s2);
+s1++;
+s2++;
+}
+return (0);
 }
---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: array
+ * @n: is the number of elements of the array
+ *
+ * Return: the array reverse
+ */
+void reverse_array(int *a, int n)
+{
+
+}
