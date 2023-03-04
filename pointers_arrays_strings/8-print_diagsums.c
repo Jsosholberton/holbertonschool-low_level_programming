b9be7bd8ce6d2e3870678012770b51c11d9af0b9
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-03 19:46:48 -0800

    diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - main entry point
+ * Description: prints the sum of the two diagonals
+ * @a: 2d array of chars
+ * @size: number matrix
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+int i;
+int c1, c2;
+c1 = 0;
+c2 = 0;
+i = 0;
+while (i < size)
+{
+c1 = c1 + *(a + i * size + i);
+c2 = c2 + *(a + i * size + size - i - 1);
+i++;
+}
+printf("%i, %i\n", c1, c2);
+}
