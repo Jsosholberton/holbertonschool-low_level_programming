30875e26ebeeea8a1527d5248bc1b2b6bd38846a
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-17 08:14:11 -0700

    main

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Descripcion: print the name of the .c
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+ 
