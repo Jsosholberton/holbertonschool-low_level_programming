d0124718bcf68588da386e2b0c9ed68e60294c4a
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-21 10:24:53 -0700

    print dog info

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,11 +9,15 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf("nil\n");
+		return;
 	}
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
 	}
 	if (d != NULL)
 	{
