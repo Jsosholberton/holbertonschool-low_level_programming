bcb79faae33a9bba8f202532f223d6753464f26e
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-21 06:47:45 -0700

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef main_dog
+#define main_dog
+/**
+ * struct dog - register the info from dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+	dog;
+
+#endif
