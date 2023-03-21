aafa4a6c7fc93c76d02d179fffe2a4583e3c4c30
Johnatan Ortiz <6403@holbertonstudents.com>
2023-03-21 11:51:12 -0700

    free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *
+ * free_dog - free memory
+ * @d: pointer of the data
  *
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
