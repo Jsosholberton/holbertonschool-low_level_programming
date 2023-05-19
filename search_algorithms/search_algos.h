5ae45d91884e40b4666799ad2766b8f4d9357084
Johnatan Ortiz <6403@holbertonstudents.com>
2023-05-19 08:18:18 -0700

    search algorithms, linear algorithm function, with betty style and running

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+/* LIBRERIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPES */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS */
